bed7bf03a17c74072b4bcf87be6d5d4c0ce015ad
vDuduardo <everbinen@gmail.com>
2022-05-23 18:51:08 -0300

    Add Create Account Activity

--- app/src/main/java/br/xpcriativa/app/CreateAccountActivity.java
+++ app/src/main/java/br/xpcriativa/app/CreateAccountActivity.java
@@ -1,8 +1,22 @@
 package br.xpcriativa.app;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
 
 public class CreateAccountActivity extends Activity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
 
+        Button createAccountButton = findViewById(R.id.buttonCreateAccount);
+
+        createAccountButton.setOnClickListener(v -> {
+
+        });
+    }
 }

0bbd4c636b07d1ddd93148649c860b7d2076f801
vDuduardo <everbinen@gmail.com>
2022-06-05 23:29:05 -0300

    Created Home Activity and profile fragment

---
+++ app/src/main/java/br/xpcriativa/app/HomeActivity.java
@@ -0,0 +1,53 @@
+package br.xpcriativa.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import br.xpcriativa.app.databinding.ActivityHomeBinding;
+import br.xpcriativa.app.fragments.HomeFragment;
+import br.xpcriativa.app.fragments.NewsFragment;
+import br.xpcriativa.app.fragments.ProfileFragment;
+import br.xpcriativa.app.fragments.SettingsFragment;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavView.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.news:
+                    replaceFragment(new NewsFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
